74e63dca6fa2d324f2d28aef1c3dbd4f7ce4fba8
shaoqiang.deng <shaoqiang.deng@mail.hypers.com>
2020-07-20 14:41:59 +0800

    brand/urls.py

--- apps/brand/urls.py
+++ apps/brand/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 
 from apps.brand import views
-from apps.brand.serializers import BrandSerializer
+from apps.brand.views import BrandModelViewSet, BrandAPIView
 
 router = routers.DefaultRouter()
-router.register('brands', BrandSerializer, basename='brands')
+router.register('brands', BrandModelViewSet, basename='brands')
 urlpatterns = [
     path('index/', views.brand_info),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('brand_data/', BrandAPIView.as_view()),
+    path('list/', views.brand_list),
 ]
