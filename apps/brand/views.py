b00c22abfcb9c031e12f67112e83d731565fda75
shaoqiang.deng <shaoqiang.deng@mail.hypers.com>
2020-07-20 14:42:50 +0800

    brand/view.py

--- apps/brand/views.py
+++ apps/brand/views.py
@@ -1,11 +1,12 @@
+from django.http import JsonResponse, HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.views import APIView
 
 from apps.brand.models import Brand
 from apps.brand.serializers import BrandSerializer
-
 
 
 @api_view(http_method_names=['GET', 'POST', 'PUT'])
@@ -30,5 +31,27 @@ def brand_info(request):
 
 
 class BrandModelViewSet(viewsets.ModelViewSet):
-    queryset = Brand
+    queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+
+
+class BrandAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        print(request.data)
+        brands = Brand.objects.all()
+        serializer = BrandSerializer(brands, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        serializer = BrandSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+def brand_list(request):
+    brandlist = Brand.objects.all().values_list(flat=True)
+    print(brandlist)
+    return HttpResponse('llll')
